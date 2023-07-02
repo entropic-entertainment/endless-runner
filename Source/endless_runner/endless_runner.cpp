8cfe7dabd3f3ddbe8710ec86a3753b115b027420
Waffels00 <84650213+Waffels00@users.noreply.github.com>
2023-07-02 16:15:27 -0500

    Updated the engine version to 5.2.1

--- Source/endless_runner/endless_runner.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "endless_runner.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, endless_runner, "endless_runner" );
- 
